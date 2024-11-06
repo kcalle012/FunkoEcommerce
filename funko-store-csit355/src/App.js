c653581fcd3fa73f9eb56508ce3c43ed5a20c24e
Kevin Calle <kcalle012@gmail.com>
2024-11-06 17:49:27 -0500

    testing

--- funko-store-csit355/src/App.js
+++ funko-store-csit355/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import {Routes, Route} from 'react-router-dom'
-
+import Layout from './Components/Layout/index'
 function App() {
   return (
     <Routes>
-      <Route>
+      <Route path="/" element={<Layout />}>  
         <Route />
         <Route />
         <Route />
